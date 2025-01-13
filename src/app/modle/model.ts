60bdfbb78d68db3c081e10b5367819bc3439607b
Amarjeet <amarjeetpatel25112002@gmail.com>
2025-01-13 17:21:41 +0530

    updatemodels

--- src/app/modle/model.ts
+++ src/app/modle/model.ts
@@ -1,1 +1,40 @@
 import mongoose ,{Schema,Document} from "mongoose";
+
+export interface Massage extends Document{
+    content:"string";
+    createdAt:Date
+}
+
+const MassageSchema :Schema<Massage>=new Schema({
+    content:{
+        type: String,
+        required:true
+    }
+    ,
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+})
+
+export interface User extends Document{
+   username:string;
+   email:string;
+   password:string;
+   verifyCodeExpiry:Date;
+   massage:Massage[];
+}
+
+const UserSchema :Schema<Massage>=new Schema({
+    content:{
+        type: String,
+        required:true
+    }
+    ,
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+})
